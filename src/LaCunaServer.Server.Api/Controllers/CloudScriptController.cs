01b3382112fa4aff9585cccd16433ded1297a077
Papaya <91090196+RavencoinGaming@users.noreply.github.com>
2022-06-04 17:13:36 +0200

    Add files via upload

---
+++ src/LaCunaServer.Server.Api/Controllers/CloudScriptController.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using LaCunaServer.Server.Api.Models.Client;
+using LaCunaServer.Server.Api.Services.Auth.Entity;
+using LaCunaServer.Server.Api.Services.CloudScript;
+
+namespace LaCunaServer.Server.Api.Controllers;
+
+[Route("CloudScript")]
+[ApiController]
+[Authorize(AuthenticationSchemes = EntityAuthenticationOptions.DefaultScheme)]
+public class CloudScriptController : Controller
+{
+    private readonly ILogger<CloudScriptController> _logger;
+    private readonly CloudScriptService _cloudScriptService;
+
+    public CloudScriptController(ILogger<CloudScriptController> logger, CloudScriptService cloudScriptService)
+    {
+        _logger = logger;
+        _cloudScriptService = cloudScriptService;
+    }
+
+    [HttpPost("ExecuteFunction")]
+    [Produces(MediaTypeNames.Application.Json)]
+    public async Task<IActionResult> ExecuteFunction(FExecuteFunctionRequest request)
+    {
+        _logger.LogDebug("Executing CloudScript function {Function}", request.FunctionName);
+
+        var result = await _cloudScriptService.ExecuteAsync(request.FunctionName, request.FunctionParameter, request.GeneratePlayStreamEvent);
+        if (result != null)
+        { 
+            return Ok(new ClientResponse<FExecuteFunctionResult>
+            {
+                Code = 200,
+                Status = "OK",
+                Data = new FExecuteFunctionResult
+                {
+                    ExecutionTimeMilliseconds = 12,
+                    FunctionName = request.FunctionName,
+                    FunctionResult = result
+                }
+            });
+        }
+
+        return StatusCode(500);
+    }
+}
